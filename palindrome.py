95b4a10d0572ac88f305620db2cfa42c152ded10
Adam Wonia <adam.wonia@gmail.com>
2021-12-12 15:59:52 +0100

    final changes

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,7 @@ def is_palindrome(text):
     """
     This function checks if the given text is a palindrome.
     :param text: Input string to check if it is a palindrome.
-    :return: Returns True if the text is a palidrome and False otherwise.
+    :return: Returns True if the text is a palindrome and False otherwise.
     """
     # Replacing all characters except for alphanumeric:
     text = "".join(ch for ch in text if ch.isalpha()).lower()
